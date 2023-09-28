2376b10f394371d6d46025ba405dc1f0e8195f52
Noithw <2907142541@qq.com>
2023-09-28 18:28:42 +0800

    优化项目

--- include/krnlib/detail/allocator.hpp
+++ include/krnlib/detail/allocator.hpp
@@ -13,34 +13,11 @@ public:
 
     using value_type = T;
 
-#if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
-    using pointer _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS = T*;
-    using const_pointer _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS = const T*;
-
-    using reference _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS = T&;
-    using const_reference _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS = const T&;
-#endif // _HAS_DEPRECATED_ALLOCATOR_MEMBERS
-
     using size_type = size_t;
     using difference_type = ptrdiff_t;
 
     using propagate_on_container_move_assignment = std::true_type;
     using is_always_equal _CXX20_DEPRECATE_IS_ALWAYS_EQUAL = std::true_type;
-
-#if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
-    template <class _Other>
-    struct _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS rebind {
-        using other = allocator<_Other>;
-    };
-
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD T* address(T& val) const noexcept {
-        return std::addressof(val);
-    }
-
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD const T* address(const T& val) const noexcept {
-        return std::addressof(val);
-    }
-#endif // _HAS_DEPRECATED_ALLOCATOR_MEMBERS
 
     constexpr allocator() noexcept {}
 
@@ -64,33 +41,6 @@ public:
         return Allocate<T>(count);
     }
 
-#if _HAS_CXX23
-    _NODISCARD_RAW_PTR_ALLOC constexpr allocation_result<T*> allocate_at_least(
-        _CRT_GUARDOVERFLOW const size_t count) {
-        return { this->allocate(count), count };
-    }
-#endif // _HAS_CXX23
-
-#if _HAS_DEPRECATED_ALLOCATOR_MEMBERS
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD_RAW_PTR_ALLOC __declspec(allocator) T* allocate(
-        _CRT_GUARDOVERFLOW const size_t count, const void*) {
-        return this->allocate(count);
-    }
-
-    template <class U, class... ArgsT>
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS void construct(U* const ptr, ArgsT&&... args) {
-        Construct<U>(ptr, std::forward<ArgsT>(args)...);
-    }
-
-    template <class U>
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS void destroy(U* const ptr) {
-        ptr->~U();
-    }
-
-    _CXX17_DEPRECATE_OLD_ALLOCATOR_MEMBERS _NODISCARD size_t max_size() const noexcept {
-        return static_cast<size_t>(-1) / sizeof(T);
-    }
-#endif // _HAS_DEPRECATED_ALLOCATOR_MEMBERS
     static constexpr size_t _Minimum_asan_allocation_alignment = std::_Asan_granularity;
 };
 }
--- include/krnlib/memory.hpp
+++ include/krnlib/memory.hpp
@@ -12,7 +12,7 @@ namespace krnlib {
 */
 template<class T>
 inline T* Allocate(size_t count) {
-    return (T*)operator new(std::_Get_size_of_n<sizeof(T)>(count));
+    return (T*)operator new(sizeof(T) * count);
 }
 
 /**
@@ -21,17 +21,6 @@ inline T* Allocate(size_t count) {
 */
 inline void Deallocate(void* ptr) {
     operator delete(ptr);
-}
-
-/**
- * @brief 对一块内存调用构造函数
- * @tparam T 对象类型
- * @param ptr 目标内存
- * @param args 构造函数参数
-*/
-template<class T, class... ArgsT>
-inline T* Construct(T* ptr, ArgsT&&... args) {
-    return ::new(std::_Voidify_iter(ptr)) T(std::forward<ArgsT>(args)...);
 }
 
 /**
